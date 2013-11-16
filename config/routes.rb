a913dbcdf308e29aea34c9cc15aeb6163843c423
Katie Ensign <katie.ensign@gmail.com>
2013-11-16 02:59:20 -0600

    added some routes.rb info

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ HackTx::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+#   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,7 +13,9 @@ HackTx::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :senders
+    root 'welcome#index'
+    match '/signup', to: 'users#new', via: 'get'
 
   # Example resource route with options:
   #   resources :products do
