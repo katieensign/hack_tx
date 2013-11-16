a96ee00943e1c20afc864c32eec873e47eaa6f1e
Kayvon Farzaneh <admin@wireless-10-146-67-92.public.utexas.edu>
2013-11-16 01:58:15 -0600

    creating new receivers?

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,8 +1,4 @@
 class PayController < ApplicationController
-    def salary(sal)
-        s = sal/26
-        @entry = Entry.new(params[:n])
-    end
     def time(prevTime)
         if(Time.now.to_date >= prevTime)
             return true
