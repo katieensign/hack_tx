0353815b401baec14288f7149f7bf118a8fdbdd4
Kayvon Farzaneh <admin@wireless-10-146-67-92.public.utexas.edu>
2013-11-16 01:52:57 -0600

    added models

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,2 +1,18 @@
 class PayController < ApplicationController
+    def salary(sal)
+        s = sal/26
+        @entry = Entry.new(params[:n])
+    end
+    def time(prevTime)
+        if(Time.now.to_date >= prevTime)
+            return true
+        else
+            return false
+        end
+    end
+    
+    def getNextPay()
+        nextPay = days_since(13)
+        nextPay #returned
+    end
 end
