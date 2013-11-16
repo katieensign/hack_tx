5abbe58882436ea80556fd5fe5eaeaafef30cae2
Katie Ensign <katie.ensign@gmail.com>
2013-11-15 20:10:45 -0600

    moving directories around

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackTx::Application.config.secret_key_base = 'a244368ebe79c2c5e50b0e872dc875f373c24b0cfbe6023a5281be772fdf96c10f5d9734f88ae837c2a73dd59903344b558f3dd01a3bb11d1f7a8792de9f3758'
