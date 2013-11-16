8c2f19871f8e863759992af96fbc9cf4976f2891
Katie Ensign <katie.ensign@gmail.com>
2013-11-16 00:44:58 -0600

    update again with the dbs

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116045915) do
+ActiveRecord::Schema.define(version: 20131116064257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "receivers", force: true do |t|
     t.integer  "sender_id"
     t.string   "email"
-    t.decimal  "salary"
+    t.decimal  "payrate"
+    t.string   "paytype"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -43,14 +37,6 @@ ActiveRecord::Schema.define(version: 20131116045915) do
     t.integer  "receiver_id"
     t.decimal  "amount"
     t.datetime "time_sent"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.decimal  "salary"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
