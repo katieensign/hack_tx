0d4e47cef12197516d3ce383641105a5e922b5b7
Kayvon Farzaneh <admin@wireless-10-146-67-92.public.utexas.edu>
2013-11-16 01:00:33 -0600

    added methods for payment and paydate

--- app/helpers/pay_helper.rb
+++ app/helpers/pay_helper.rb
@@ -1,2 +1,18 @@
 module PayHelper
+    def salary(sal)
+        sal = sal/26
+        sal
+    end
+    def time(prevTime)
+        if(Time.now.to_date >= prevTime)
+            return true
+            else
+            return false
+        end
+    end
+    
+    def getNextPay()
+        nextPay = days_since(13)
+        nextPay #returned
+    end
 end
